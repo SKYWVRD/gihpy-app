0407d54839b8e7418a42e9ca8931773a728f0dd5
Sean Boonzaier <boonzaiersa@gmail.com>
2022-09-17 14:38:54 +0200

    Add button to refresh gif

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+const newCatButton = document.getElementById('new-cat-button')
+fetchGif()
+
+
 const img = document.querySelector('img');
-            fetch('https://api.giphy.com/v1/gifs/translate?api_key=i13s2h9tf8ZI6sj0JMPaOcBBZpZylSM5&s=cats', {mode: 'cors'})
-                .then(function(response){
-                    return response.json();
-                })
-                .then(function(response){
-                    img.src = response.data.images.original.url;
-                });
+function fetchGif () {
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=i13s2h9tf8ZI6sj0JMPaOcBBZpZylSM5&s=cats', {mode: 'cors'})
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(response){
+                img.src = response.data.images.original.url;
+            });
+} 
+
+
+newCatButton.addEventListener('click', fetchGif);           
