d88a769e46a87479901dc1cf2065b5860f74ee90
Sean Boonzaier <boonzaiersa@gmail.com>
2022-09-17 14:57:40 +0200

    Add no image error handling

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const newCatButton = document.getElementById('new-cat-button')
-fetchGif()
+const newCatButton = document.getElementById('new-cat-button');
+const searchBox = document.getElementById('search-text');
+fetchGif('Default');
 
 
 const img = document.querySelector('img');
-function fetchGif () {
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=i13s2h9tf8ZI6sj0JMPaOcBBZpZylSM5&s=cats', {mode: 'cors'})
+function fetchGif (searchParameter) {
+    
+        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=i13s2h9tf8ZI6sj0JMPaOcBBZpZylSM5&s=${searchParameter}`, {mode: 'cors'})
             .then(function(response){
                 return response.json();
             })
             .then(function(response){
                 img.src = response.data.images.original.url;
-            });
-} 
+            })
+            .catch(() => {
+                img.src = 'https://media3.giphy.com/media/l1J9EdzfOSgfyueLm/giphy.gif?cid=ecf05e47ilucmas9r4xlgzkl2oqv7i6nbw1ch3utuzugu5go&rid=giphy.gif&ct=g'
+            })
+};
 
 
-newCatButton.addEventListener('click', fetchGif);           
+newCatButton.addEventListener('click', ()=>{
+    fetchGif(searchBox.value)
+});           
