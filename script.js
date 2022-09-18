53441706cc24f56e1089d1730bf71759584aac03
Sean Boonzaier <boonzaiersa@gmail.com>
2022-09-18 10:54:37 +0200

    Update chained promimses to async functions

--- script.js
+++ script.js
@@ -4,18 +4,14 @@ fetchGif('Default');
 
 
 const img = document.querySelector('img');
-function fetchGif (searchParameter) {
-    
-        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=i13s2h9tf8ZI6sj0JMPaOcBBZpZylSM5&s=${searchParameter}`, {mode: 'cors'})
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(response){
-                img.src = response.data.images.original.url;
-            })
-            .catch(() => {
-                img.src = 'https://media3.giphy.com/media/l1J9EdzfOSgfyueLm/giphy.gif?cid=ecf05e47ilucmas9r4xlgzkl2oqv7i6nbw1ch3utuzugu5go&rid=giphy.gif&ct=g'
-            })
+async function fetchGif (searchParameter) {
+    try{
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=i13s2h9tf8ZI6sj0JMPaOcBBZpZylSM5&s=${searchParameter}`, {mode: 'cors'})
+        const gifData = await response.json();
+        img.src = gifData.data.images.original.url    
+    } catch (error) {
+        img.src = 'https://media3.giphy.com/media/l1J9EdzfOSgfyueLm/giphy.gif?cid=ecf05e47ilucmas9r4xlgzkl2oqv7i6nbw1ch3utuzugu5go&rid=giphy.gif&ct=g'
+    }
 };
 
 
